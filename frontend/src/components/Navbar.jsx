da2880f78fe0d6c216ae53965d6b5e8a308d4ce9
Athul3588krishna <athul98517772@gmail.com>
2025-01-28 14:25:26 +0530

    updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { assets } from '../assets/assets';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const [visible,setVisible]=useState(false);
+
   return (
     <div className='flex items-center justify-between py-5 font-medium'>
       <img src={assets.logo} className='w-36' alt='not available' />
@@ -36,6 +39,16 @@ const Navbar = () => {
             </div>
           </div>
         </div>
+        <Link to='/cart' className='relative'>
+        <img src={assets.cart_icon} className='w-5' alt=""/>
+        <p className='absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]'>10</p>
+        </Link>
+        <img onClick={()=>setVisible(true)} src={assets.menu_icon} className='w-5 cursor-pointer sm:hidden' alt=""/>
+      </div>
+
+      {/*/Sidebar menu for small screens pending yet*/}
+      <div className={'absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${visible?}'}>
+
       </div>
     </div>
   );
