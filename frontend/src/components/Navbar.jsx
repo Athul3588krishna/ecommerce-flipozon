65734bae8bc3455abde6262f03ea70d8c1812735
Athul3588krishna <athul98517772@gmail.com>
2025-01-28 14:08:16 +0530

    update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import { assets } from '../assets/assets'
-import { NavLink } from 'react-router-dom'
+import React from 'react';
+import { assets } from '../assets/assets';
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
@@ -25,12 +25,20 @@ const Navbar = () => {
         </NavLink>
       </ul>
       <div className='flex items-center gap-6'>
-        <img src={assets.search_icon} className='w-5 cursor-pointer' alt=''>
-        </img>
+        <img src={assets.search_icon} className='w-5 cursor-pointer' alt='' />
+        <div className='relative group'>
+          <img className='w-5 cursor-pointer' src={assets.profile_icon} alt="" />
+          <div className='absolute hidden group-hover:block right-0 pt-4'>
+            <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+              <p className='cursor-pointer hover:text-black'>My profile</p>
+              <p className='cursor-pointer hover:text-black'>Orders</p>
+              <p className='cursor-pointer hover:text-black'>Logout</p>
+            </div>
+          </div>
+        </div>
       </div>
-
     </div>
-  )
+  );
 }
 
 export default Navbar;
